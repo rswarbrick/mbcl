20e7524d156b767ab1ba4c8300bfc3e64194f0d2
Rupert Swarbrick <rswarbrick@gmail.com>
2011-12-29 19:28:18 +0000

    Debug web calls.

--- mbws.lisp
+++ mbws.lisp
@@ -12,6 +12,9 @@
   "The UT of the last call to the web service. Used to ensure that we don't make
 more than one query per second.")
 
+(defvar *debug-mbws-calls* t
+  "Print something to the console on each web service call.")
+
 (defun mb-body-format (headers external-format)
   (multiple-value-bind (type subtype parameters)
       (drakma:get-content-type headers)
@@ -28,6 +31,10 @@ more than one query per second.")
 MBID (ie for search calls, just pass NIL). Returns the parsed XML."
   (unless (and (or (null mbid) (stringp mbid)) (stringp method))
     (error "METHOD must be a string and MBID must be a string or NIL."))
+  (when *debug-mbws-calls*
+    (format t "MBWS-CALL: ~A / ~A / ~A~%" method mbid parameters)
+    (force-output))
+
   (when *last-call*
     (when (= 0 (- (get-universal-time) *last-call*))
       (sleep 1)))
@@ -38,4 +45,7 @@ MBID (ie for search calls, just pass NIL). Returns the parsed XML."
           (format nil "~A~A~@[/~A~]" +mb-search-base+ method mbid)
           :parameters parameters
           :user-agent +mb-user-agent+)))
-    (setf *last-call* (get-universal-time))))
+    (setf *last-call* (get-universal-time))
+    (when *debug-mbws-calls*
+      (format t "END MBWS-CALL~%")
+      (force-output))))
