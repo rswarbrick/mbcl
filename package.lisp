d1a6f7ba56ca0bf58f73f08f64284b7b57730c73
Rupert Swarbrick <rswarbrick@gmail.com>
2011-12-30 12:46:38 +0000

    Export symbols.

--- package.lisp
+++ package.lisp
@@ -1,2 +1,56 @@
 (defpackage :mbcl
-  (:use :cl))
+  (:use :cl)
+  (:export
+   ;; Search
+   #:search-request
+
+   ;; General MB call
+   #:mb-request #:*mb-slot-value-debug*
+
+   ;; MB Objects & accessors
+   #:artist
+     #:id #:name #:sort-name #:disambiguation #:artist-type
+     #:country #:life-span #:aliases
+   #:release-group
+     #:id #:rg-type #:title #:first-release-date
+     #:artist-credit #:release-list
+   #:release
+     #:id #:title #:disambiguation #:status #:quality #:packaging
+     #:text-representation #:artist-credit #:release-group #:date
+     #:country #:asin #:barcode #:label-info #:medium-list
+   #:recording
+     #:id #:title #:length #:artist-credit #:release-list
+   #:label
+     #:id #:name #:sort-name #:disambiguation #:label-code
+     #:label-type #:country #:life-span #:aliases
+   #:work
+     #:id #:work-type #:title #:disambiguation #:aliases
+
+   ;; Auxiliary objects & accessors
+   #:name-credit
+     #:name-credit-string #:artist #:name #:join-phrase
+   #:alias
+     #:locale
+   #:time-period
+     #:begin #:end
+   #:list-segment
+     #:ls-type #:ls-count #:ls-offset #:contents
+   #:artist-credit
+     #:name-credits
+   #:track
+     #:title
+   #:disc
+     #:id #:sectors #:release-list
+   #:medium
+     #:position #:format #:disc-list #:track-list
+   #:medium-list
+     #:track-count
+   #:text-representation
+     #:language #:script
+   #:label-info
+     #:catno #:label
+   #:relation
+     #:relation-type #:direction #:attributes
+     #:beginning #:end #:target
+   #:attribute-list
+     #:attributes))
