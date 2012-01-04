3150451db8da45d3559ba6798a1846d9b4644833
Rupert Swarbrick <rswarbrick@gmail.com>
2012-01-04 13:01:07 +0000

    Fix silly bug in inc-should-be-marked-updated?

--- hilevel-webservice.lisp
+++ hilevel-webservice.lisp
@@ -29,8 +29,8 @@
                                (find-slot-definition object key))))
                 (or (null slot-inc)
                     (and (stringp slot-inc)
-                         (string= "" slot-inc)
-                         (string= inc slot-inc))))))))
+                         (or (string= "" slot-inc)
+                             (string= inc slot-inc)))))))))
 
 (defun set-inc-updated! (object inc)
   "Modifies OBJECT by updating the ULIST such that any slot that gets picked up
